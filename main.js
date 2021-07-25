16353643b78e0b1873040da66a176d23d836279d
arynthernium <69891576+arynthernium@users.noreply.github.com>
2021-07-25 01:08:18 -0400

    finish

---
+++ main.js
@@ -0,0 +1,7 @@
+function run() {
+	if (Math.random() > 0.5) {
+		document.getElementById('thespoon').style.fill = "#ddd";
+	} else {
+		document.getElementById('thespoon').style.fill = "#111";
+	}
+};
